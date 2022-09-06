6368bf7793973d86b56aa11bddea0da337b07158
lab6TranTrungKien <19130110@st.hcmuaf.edu.vn>
2022-09-07 01:41:09 +0700

    first commit

---
+++ db.js
@@ -0,0 +1,32 @@
+const products = require("./data/products");
+
+
+module.exports = {
+  products
+  
+}
+
+
+// const items = require("./data/items");
+
+// module.exports = () => {
+
+//   var tags = items.map((i) => i.tags);
+//   tags = [].concat.apply([], tags);
+//   tags = [...new Set(tags)];
+
+ 
+
+//   var brands = items.map((i) => i.manufacturer);
+//   brands = [...new Set(brands)];
+
+//   var itemTypes = items.map((i) => i.itemType);
+//   itemTypes = [...new Set(itemTypes)];
+
+//   return {
+//     items,
+//     tags,
+//     brands,
+//     itemTypes,
+//   };
+// };
